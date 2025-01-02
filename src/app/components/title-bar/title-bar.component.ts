a35e202e6fafd043fd8334ed5a39956570b8c009
Lucas Oliveira <lucaoxt@gmail.com>
2025-01-02 15:06:34 -0300

    initial structure

---
+++ src/app/components/title-bar/title-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-bar',
+  imports: [],
+  templateUrl: './title-bar.component.html',
+  styleUrl: './title-bar.component.css'
+})
+export class TitleBarComponent {
+
+}
