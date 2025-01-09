a690ba569c057b9aab84648efba2f225b50c966f
Lucas Oliveira <lucaoxt@gmail.com>
2025-01-09 16:09:57 -0300

    content change

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,14 +8,20 @@ export type Post = {
 export const dataFake: Post[] = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Flatpak 1.16: Novidades e Melhorias na Distribuição de Apps",
+    "description": "A versão Flatpak 1.16 trouxe melhorias como suporte a mais distribuições Linux, segurança aprimorada com sandboxing, melhor desempenho de aplicativos e integração com novas ferramentas de desenvolvimento, visando uma experiência mais robusta e acessível para usuários e desenvolvedores no Linux.",
+    "photoCover":"https://www.edivaldobrito.com.br/wp-content/uploads/2025/01/flatpak-linux.webp"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "KDE Plasma 6.3: Novidades e Recursos na Versão Beta",
+    "description": "KDE Plasma 6.3 oferece melhorias significativas como uma interface aprimorada, desempenho otimizado, novos recursos de personalização, melhorias na acessibilidade e melhor integração com aplicativos, proporcionando uma experiência de usuário mais fluida e intuitiva para um ambiente de desktop moderno.",
+    "photoCover":"https://www.edivaldobrito.com.br/wp-content/uploads/2025/01/kde-plasma-63-novidades-e-recursos-na-versao-beta.webp"
+  },
+  {
+    "id":"3",
+    "title": "Como instalar o bonito tema OS X Mavericks no Linux",
+    "description": "Se você gostava do visual do OS X 10.9 e quer experimentá-lo no sistema do pinguim, veja como instalar o bonito tema OS X Mavericks no Linux. O OS X Mavericks (versão 10.9) foi a décima versão do OS X, da Apple, um sistema operacional para desktop e servidor para computadores Macintosh.",
+    "photoCover":"https://www.edivaldobrito.com.br/wp-content/uploads/2018/03/tema-os-x-mavericks-no-linux.jpg"
   }
 ]
